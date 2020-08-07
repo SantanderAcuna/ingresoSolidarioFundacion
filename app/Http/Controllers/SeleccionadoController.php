1f5e522f384642ef2f6ec044503fb02c92984866
Santander acuña <panson@misena.edu.co>
2020-08-07 15:54:33 -0500

    se cambia nombre a la bd

--- app/Http/Controllers/SeleccionadoController.php
+++ app/Http/Controllers/SeleccionadoController.php
@@ -8,14 +8,6 @@ use Illuminate\Http\Request;
 class SeleccionadoController extends Controller
 {
 
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
-
-
-
     /**
      * Display a listing of the resource.
      *
